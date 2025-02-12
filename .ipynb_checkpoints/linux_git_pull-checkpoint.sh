e28fc8829a7b0c37ef1e920e723e8d495ff77113
dhang7 <d1799215362@162.com>
2025-02-12 15:59:21 +0800

    commit from linux

---
+++ .ipynb_checkpoints/linux_git_pull-checkpoint.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+clear
+echo "开始执行"
+# 该脚本功能是清除本地的更改，拉取新的代码，主要用于服务器上
+
+# 撤销本地工作区的更改
+git restore .
+
+# 清空暂存区的更改
+git restore --staged .
+
+# 删除本地新增的未跟踪文件和目录
+git clean -fd
+
+# 拉取新的代码
+git pull
