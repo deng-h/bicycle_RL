634ea84a0a09c3287bce54227554fd322c2fc796
dh <1799215362@qq.com>
2025-02-12 15:45:25 +0800

    1

---
+++ ppo_config_linux.py
@@ -0,0 +1,16 @@
+from utils.my_feature_extractor import MyFeatureExtractorLidar
+
+hyperparams = {
+    "BicycleMazeLidar2-v0": dict(
+        policy="MultiInputPolicy",
+        normalize=dict(norm_obs=True, norm_reward=False, norm_obs_keys=['lidar', 'obs']),
+        n_envs=32,
+        n_timesteps=300000,
+        learning_rate=1e-4,
+        policy_kwargs=dict(
+            features_extractor_class=MyFeatureExtractorLidar,
+            net_arch=dict(pi=[512, 512], vf=[512, 512])
+        ),
+        # monitor_kwargs=dict(info_keywords=('flywheel_vel',))
+    )
+}
--- train.sh
+++ train.sh
@@ -3,6 +3,16 @@
 clear
 echo "开始执行"
 
+# 检测操作系统
+OS=$(uname)
+
+# 根据操作系统设置配置文件名称
+if [ "$OS" = "Linux" ]; then
+    CONF_FILE="ppo_config_linux"
+else
+    CONF_FILE="ppo_config"
+fi
+
 # 平衡训练
 #python train.py \
 #--algo ppo \
@@ -16,10 +26,10 @@ echo "开始执行"
 python train.py \
 --algo ppo \
 --env BicycleMazeLidar2-v0 \
---conf-file ppo_config \
+--conf-file "$CONF_FILE" \
 --vec-env subproc \
 --progress \
---device cpu
+--device cpu \
 --trained-agent ./logs/ppo/BicycleMazeLidar2-v0_3/best_model.zip
 
 #--tensorboard-log ./logs/tensorboard/
